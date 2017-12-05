21161bdd358bc862703bd899b0f74a16a0195736
kanna <kbrownjamaica@hotmail.com>
2017-12-05 12:21:29 -0500

    submitting project for review

--- designs.js
+++ designs.js
@@ -2,36 +2,31 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-$('#sizePicker').submit(function() {
+$('#sizePicker').submit(function(e) {
+  e.preventDefault();
   makeGrid();
-  return false;
 });
 
 function makeGrid() {
 // Creates a grid for the pixel maker
+  const table = document.getElementById('pixel_canvas');
   const inputHeight = parseInt($('#input_height').val());
   const inputWidth = parseInt($('#input_width').val());
-  let canvas = "";
-
   for (let i = 0; i < inputHeight; i++) {
-    canvas += "<tr>";
+    const row = table.insertRow(i);
     for (let k = 0; k < inputWidth; k++) {
-      canvas += "<td></td>";
+      const cell = row.insertCell(k);
+      //changes colour when a square is clicked and removes colour when clicked again
+      cell.addEventListener('click', function() {
+        const colorPicker = $('#colorPicker').val();
+        if ($(this).hasClass('checked')) {
+          $(this).css('background-color', 'inherit');
+          $(this).removeClass('checked');
+        } else {
+          $(this).css('background-color', colorPicker);
+          $(this).addClass('checked');
+        }
+      });
     }
-    canvas += "</tr>";
   }
-
-  $('#pixel_canvas').html(canvas);
-  canvas="";
 }
-
-//changes colour when a square is clicked and removes colour when clicked again
-$(document).on("click", "#pixel_canvas td", function() {
-  const colorPicker = $('#colorPicker').val();
-  if ($(this).hasClass('checked')) {
-    $(this).css('background-color', 'inherit');
-  } else {
-    $(this).css('background-color', colorPicker);
-    $(this).addClass('checked');
-  }
-});
